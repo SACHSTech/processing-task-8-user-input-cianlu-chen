c4cfe6ac17c28f6d8dea396a8434879bd0c3def0
cianlu-chen <kalina.chen25@ycdsbk12.ca>
2023-12-12 00:03:23 -0500

    cake?

--- Sketch.java
+++ Sketch.java
@@ -1,21 +1,42 @@
 import processing.core.PApplet;
 import processing.core.PImage;
+import java.awt.event.MouseEvent;
 
 public class Sketch extends PApplet {
   // Images
-  PImage imgBackgroud;
+  PImage imgBackground;
+  PImage imgCake;
+  PImage imgCream;
 	
   public void settings() {
-    size(800, 800);
+    size(750, 750);
   }
 
   public void setup() {
-    background(210, 255, 173);
+    imgBackground = loadImage("background.jpg");
+    imgBackground.resize(750, 750);
+
+    imgCake = loadImage("cake.png");
+    imgCake.resize(400, 400);
+
+    imgCream = loadImage("cream.png");
+    imgCream.resize(50, 50);
+
+    image(imgBackground, 0, 0);
+    image(imgCake, 175, 145);
   }
 
   public void draw() {
-	  
+    if(mousePressed){
+      noStroke();
+      fill(200, 250, 250);
+      ellipse(mouseX, mouseY, 20, 20);
+    }
   }
-  
-  // define other methods down here.
+
+  public void mouseClicked(){
+    if(true){
+      image(imgCream, mouseX - 25, mouseY - 25);
+    }
+  }
 }
